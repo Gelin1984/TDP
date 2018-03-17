244bc66e835da1f22bc69c60eeb57d27add29c23
Greg <gregory.dopart@gmail.com>
2018-03-17 21:40:48 +0800

    5.3 adding e-mail

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,26 +1,29 @@
 class HomeController < ApplicationController
-  def index
-  end
+def index
+end
 
-  def about
-  end
+def about
+end
 
-  def landing_page
-    @products = Product.limit(3)
+def landing_page
+@products = Product.limit(3)
 
-  end
+end
 
-  def contact
-  end
+def contact
+end
 
-  def faq
-  end
+def faq
+end
 
-  def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
-    UserMailer.contact_form(@email, @name, @message).deliver_now
-    end
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(from: @email,
+      to: 'your-email@example.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+end
 
-    end
+end
