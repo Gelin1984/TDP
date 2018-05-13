b6232160192bffdfc6c28677efae7b4900180075
Greg <gregory.dopart@gmail.com>
2018-05-13 16:21:36 +0800

    6.14 Advanced Action Cable

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -1,9 +1,13 @@
-def perform(comment, current_user)
-  ProductChannel.broadcast_to(comment.product_id, comment: render_comment(comment, current_user), average_rating: comment.product.average_rating)
-end
+class CommentUpdateJob < ApplicationJob
+  queue_as :default
 
-private
+  def perform(comment, current_user)
+    ProductChannel.broadcast_to(comment.product_id, comment: render_comment(comment, current_user), average_rating: comment.product.average_rating)
+  end
+
+  private
 
   def render_comment(comment, current_user)
     CommentsController.render(partial: 'comments/comment', locals: { comment: comment, current_user: current_user })
   end
+end
